5d1ea6c44b3ec344d4d3bd17635a9c9c4b0144fb
Arburator <garyoakheart@gmail.com>
2016-02-14 20:52:59 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
+//The stupidity script
+var Stupid = {
+  GayMan: "Adam Snowden",
+  Hooker: "His Mom"
+},
 
+sys.sendAll(+ GayMan + " banged " + Hooker + ", thats how he joined PO, by playing with his donger on the keyboard with " + Hooker + ".");
